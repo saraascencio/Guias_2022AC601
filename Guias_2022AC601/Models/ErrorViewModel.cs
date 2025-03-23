26c13aba1cc29422f104b3a74d254cc2fc698a1f
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-03-23 08:50:02 -0600

    Agregar archivos de proyecto.

---
+++ Guias_2022AC601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Guias_2022AC601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
